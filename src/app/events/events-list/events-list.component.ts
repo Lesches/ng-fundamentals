7dcaf10fe6b71c5f26e81f527f592b25cb638932
Lesches <sholomls@gmail.com>
2020-06-17 19:55:00 -0400

    events list

--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -6,7 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events-list.component.css']
 })
 export class EventsListComponent implements OnInit {
-
+event = {
+  id: 1,
+  name: 'Angular Contact',
+  date: '13/6/2020',
+  time: '10:00 AM',
+  price: 599.99,
+  imageUrl: '/assets/images/angularconnect-sheild.png',
+  location: {
+    address: '1057 DT',
+    city: 'London',
+    country: 'England'
+  }
+} 
   constructor() { }
 
   ngOnInit(): void {
